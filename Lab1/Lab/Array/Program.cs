316d67b4dd584960961f93bc5d7d28b60a7e2a4c
nastasya3875 <61266529+nastasya3875@users.noreply.github.com>
2021-04-22 11:30:24 +0300

    END HW

---
+++ Lab1/Lab/Array/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            Console.WriteLine("Введите пять чисел");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("{0}-е число: ", i + 1);
+                arr[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            int newper;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        newper = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = newper;
+                    }
+                }
+            }
+
+            Console.WriteLine("Вывод массива");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab1/Lab/Calc/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double total;
+            char sign;
+
+            Console.WriteLine("Введите число: ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите оператор (+, -, *,/): ");
+            sign = Convert.ToChar(Console.ReadLine());
+
+            Console.WriteLine("Введите число: ");
+            b = Convert.ToDouble(Console.ReadLine());
+
+            if (sign == '+')
+            {
+                total = a + b;
+                Console.WriteLine("Ответ: " + total);
+            }
+            else if (sign == '-')
+            {
+                total = a - b;
+                Console.WriteLine("Ответ: " + total);
+            }
+            else if (sign == '*')
+            {
+                total = a * b;
+                Console.WriteLine("Ответ: " + total);
+            }
+            else if (sign == '/')
+            {
+                total = a / b;
+                Console.WriteLine("Ответ: " + total);
+            }
+            else
+            {
+                Console.WriteLine("Неизвестный оператор");
+            }
+
+        }
+    }
+}
+
