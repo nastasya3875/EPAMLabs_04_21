b46165bc5e6fc2a94ca3c91c27dadafbb12b1431
nastasya3875 <61266529+nastasya3875@users.noreply.github.com>
2021-05-04 19:20:43 +0300

    END HW5

---
+++ Lab5/LabTest/LabTest/TestSquare.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTest
+{
+    class TestSquare
+    {
+        [Test]
+        public void TestArea([Range(0, 20)] int x)
+        {
+            var temp = GeometricFigures.Square.GetArea(x);
+            Assert.IsTrue(x * x == temp, $"Мы ожидаем {x * x}, а пришло {temp}");  
+        }
+
+        //18, 7
+
+        [Test]
+        public void TestLength([Range(0, 20)] int x)
+        {
+            var temp = GeometricFigures.Square.GetLength(x);
+            Assert.IsTrue(x * 4 == temp, $"Мы ожидаем {x * 4}, а пришло {temp}");  
+        }
+
+        //6, 9
+    }
+}
---
+++ Lab5/LabTest/LabTest/TestTriangle.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTest
+{
+    class TestTriangle
+    {
+        [Test]
+        public void TestArea([Range(0, 20)] double x, [Range(0, 20)] double y, [Range(0, 20)] double z)
+        {
+            var temp = GeometricFigures.Triangle.GetArea(x, y, z);
+            double p = (x + y + z) / 2;
+            var temp2 = Math.Sqrt(p * (p - x) * (p - y) * (p - z));
+            Assert.IsTrue(temp2 == temp, $"Мы ожидаем {temp2}, а пришло {temp}");
+        }
+
+        [Test]
+        public void TestLength([Range(0, 20)] double x, [Range(0, 20)] double y, [Range(0, 20)] double z)
+        {
+            var temp = GeometricFigures.Triangle.GetLength(x, y, z);
+            Assert.IsTrue((x + y + z) == temp, $"Мы ожидаем {(x + y + z)}, а пришло {temp}");
+        }
+
+        // здесь очень много не выполненных тестов
+
+    }
+}
